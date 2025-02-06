39f969fcd3856337477747c12f900051441caf78
ProsperCoded <enweremproper@gmail.com>
2025-02-06 11:20:11 +0100

    Improve JWT verification in JWTGuard by adding error handling and logging for missing tokens

--- backend/src/auth/jwt.guard.ts
+++ backend/src/auth/jwt.guard.ts
@@ -8,20 +8,33 @@ import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
 import { JWT_IDENTIFIER } from 'src/config/jwt.config';
+
 @Injectable()
 export class JWTGuard implements CanActivate {
   constructor(
     @Inject(JWT_IDENTIFIER) private readonly jwtService: JwtService,
   ) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest() as Request;
     console.log('cookies', request.cookies);
-    const decoded = this.jwtService.verify(request.cookies.Authorization);
-    console.log({ decoded });
-    if (!decoded) return false;
-    request.user = { id: decoded.sub };
-    return !!decoded;
+
+    const token = request.cookies.Authorization;
+    if (!token) {
+      console.error('JWT token is missing in cookies');
+      return false;
+    }
+
+    try {
+      const decoded = this.jwtService.verify(token);
+      console.log({ decoded });
+      request.user = { id: decoded.sub };
+      return !!decoded;
+    } catch (error) {
+      console.error('Error verifying JWT token:', error.message);
+      return false;
+    }
   }
 }
